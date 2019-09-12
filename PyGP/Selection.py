704cd4de2eea74364f890910c19860641e7257dd
NateRiz <riziknat@msu.edu>
2019-09-11 20:34:16 -0400

    elite

--- PyGP/Selection.py
+++ PyGP/Selection.py
@@ -14,6 +14,19 @@ def tournament(hws):
     return next_gen
 
 
+def elite(hws, k):
+    next_gen = []
+    inst_lib = hws[0].inst_lib
+    hws.sort(key=lambda hw: hw.fitness, reverse=True)
+    [next_gen.append(copy(hw) for hw in hws[:k])]
+    while len(next_gen) < len(hws):
+        hw = Hardware(inst_lib)
+        hw.generate_program()
+        next_gen.append(hw)
+
+
+
+
 def copy(src: Hardware):
     hw = Hardware(src.inst_lib)
 
