2004ed620fb893057a6238c573a9aac20e2bae63
NateRiz <riziknat@msu.edu>
2019-09-12 19:04:11 -0400

    fixed instructions per bird

--- CustomInstructions.py
+++ CustomInstructions.py
@@ -1,29 +1,34 @@
 from time import sleep
 
-def jump(game):
-    game.jump()
+
+def jump(hardware, game):
+    game.jump(hardware.id)
     return 0
+
 
 def get_bird_height(hardware, args, game):
     if args[0] < 0 or args[0] >= len(hardware.registers): return 0
-    hardware.registers[args[0]] = game.bird.y
+    hardware.registers[args[0]] = game.birds[hardware.id].rect.y
     return 0
+
 
 def get_gap_top(hardware, args, game):
     if args[0] < 0 or args[0] >= len(hardware.registers): return 0
     for pipe in game.pipes:
-        if pipe.top.x > game.bird.right:
+        if pipe.top.x > game.birds[hardware.id].rect.right:
             hardware.registers[args[0]] = pipe.top.bottom
             break
     return 0
 
+
 def get_gap_bot(hardware, args, game):
     if args[0] < 0 or args[0] >= len(hardware.registers): return 0
     for pipe in game.pipes:
-        if pipe.top.x > game.bird.right:
+        if pipe.top.x > game.birds[hardware.id].rect.right:
             hardware.registers[args[0]] = pipe.bot.top
             break
     return 0
+
 
 def wait():
     sleep(.25)
